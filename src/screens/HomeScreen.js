700a942b7b58c255212e07cf0c96bee8da5a6b42
cohlerCoded <jeffcohler@cohlercoded.com>
2021-07-19 16:49:56 -0700

    install expo local/ test changes

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,14 +1,14 @@
-import React from "react";
-import { Text, StyleSheet } from "react-native";
+import React from 'react'
+import { Text, StyleSheet } from 'react-native'
 
 const HomeScreen = () => {
-  return <Text style={styles.text}>Hi there!</Text>;
-};
+  return <Text style={styles.text}>Testing</Text>
+}
 
 const styles = StyleSheet.create({
   text: {
-    fontSize: 30
-  }
-});
+    fontSize: 30,
+  },
+})
 
-export default HomeScreen;
+export default HomeScreen
