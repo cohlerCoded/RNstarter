d4c331cfbac09feb91a0b65ff6f482ea48fb5d25
cohlerCoded <jeffcohler@cohlercoded.com>
2021-07-20 13:57:32 -0700

    Buttons in action

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
-import { Text, StyleSheet } from 'react-native'
+import { Text, StyleSheet, View, Button } from 'react-native'
 
 const HomeScreen = () => {
-  return <Text style={styles.text}>Testing</Text>
+  return (
+    <View>
+      <Text style={styles.text}>Home Screen</Text>
+      <Button title='Components Demo' onPress={() => console.log('Pressed')} />
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
