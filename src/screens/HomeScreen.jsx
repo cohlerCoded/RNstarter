d270bf89b865a1e9082f3a484fe258c920f32027
cohlerCoded <jeffcohler@cohlercoded.com>
2021-07-20 14:29:22 -0700

    more destructuring/ button refactor

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,17 +1,13 @@
 import React from 'react'
 import { Text, StyleSheet, View, Button, TouchableOpacity } from 'react-native'
 
-const HomeScreen = (props) => {
-  const { navigate } = props.navigation
+const HomeScreen = ({ navigation }) => {
+  const { navigate } = navigation
   return (
     <View>
       <Text style={styles.text}>Home Screen</Text>
       <Button title='Components Demo' onPress={() => navigate('Components')} />
-      <TouchableOpacity onPress={() => navigate('List')}>
-        <Text>List Demo</Text>
-        <Text>List Demo</Text>
-        <Text>List Demo</Text>
-      </TouchableOpacity>
+      <Button title='List Demo' onPress={() => navigate('List')} />
     </View>
   )
 }
