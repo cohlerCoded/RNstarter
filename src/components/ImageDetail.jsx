b6ad5740994d702ad69cc394f9de45e6d09cd444
cohlerCoded <jeffcohler@cohlercoded.com>
2021-07-20 15:04:52 -0700

    parent-child relationships

---
+++ src/components/ImageDetail.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { View, Text, StyleSheet, Image } from 'react-native'
+
+const ImageDetail = () => {
+  return <Text>Image Detail</Text>
+}
+
+const styles = StyleSheet.create({})
+export default ImageDetail
