3addd9b6b9cf96f30c392130f59435c7ab9e206d
cohlerCoded <jeffcohler@cohlercoded.com>
2021-07-19 23:25:24 -0700

    some FlatList props

--- src/screens/ListScreen.jsx
+++ src/screens/ListScreen.jsx
@@ -16,15 +16,22 @@ const ListScreen = () => {
   ]
   return (
     <FlatList
+      horizontal
+      showsHorizontalScrollIndicator={false}
       keyExtractor={(friend) => friend.name}
       data={friends}
       renderItem={({ item }) => {
-        return <Text>{item.name}</Text>
+        return <Text style={styles.textStyle}>{item.name}</Text>
       }}
     />
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  textStyle: {
+    marginHorizontal: 50,
+    marginVertical: 50,
+  },
+})
 
 export default ListScreen
