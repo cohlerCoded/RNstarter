f06ec298206bc877f55ec78b695425b32338a374
cohlerCoded <jeffcohler@cohlercoded.com>
2021-07-21 10:00:23 -0700

    showing images as props url

--- src/components/ImageDetail.jsx
+++ src/components/ImageDetail.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import { View, Text, StyleSheet, Image } from 'react-native'
 
-const ImageDetail = ({ title }) => {
-  return <Text>{title}</Text>
+const ImageDetail = ({ title, imageURL, imagePath }) => {
+  return (
+    <View>
+      <Text>{title}</Text>
+      <Image source={{ uri: imageURL }} style={styles.imageStyle} />
+    </View>
+  )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  imageStyle: { width: '100%', height: 200, resizeMode: 'stretch' },
+})
 export default ImageDetail
--- src/screens/ImageScreen.jsx
+++ src/screens/ImageScreen.jsx
@@ -6,9 +6,18 @@ const ImageScreen = () => {
   return (
     <View>
       <Text>Image Screen</Text>
-      <ImageDetail title='Forest' />
-      <ImageDetail title='Beach' />
-      <ImageDetail title='Mountain' />
+      <ImageDetail
+        title='Forest'
+        imageURL='https://images.unsplash.com/photo-1448375240586-882707db888b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80'
+      />
+      <ImageDetail
+        title='Beach'
+        imageURL='https://images.unsplash.com/photo-1508313157893-34fe6176c189?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=968&q=80'
+      />
+      <ImageDetail
+        title='Mountain'
+        imageURL='https://images.unsplash.com/photo-1519681393784-d120267933ba?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'
+      />
     </View>
   )
 }
