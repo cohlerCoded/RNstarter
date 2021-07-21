75f23596fadcf5ca8165a7968dbc93deabf5d533
cohlerCoded <jeffcohler@cohlercoded.com>
2021-07-21 10:14:43 -0700

    conditional rendering cleanup

--- src/components/ImageDetail.jsx
+++ src/components/ImageDetail.jsx
@@ -2,21 +2,15 @@ import React from 'react'
 import { View, Text, StyleSheet, Image } from 'react-native'
 
 const ImageDetail = ({ title, imageURL, imagePath }) => {
-  if (imagePath) {
-    return (
-      <View>
-        <Text>{title}</Text>
-        <Image source={imagePath} style={styles.imageStyle} />
-      </View>
-    )
-  } else {
-    return (
-      <View>
-        <Text>{title}</Text>
-        <Image source={{ uri: imageURL }} style={styles.imageStyle} />
-      </View>
-    )
-  }
+  return (
+    <View>
+      <Image
+        source={imagePath ? imagePath : { uri: imageURL }}
+        style={styles.imageStyle}
+      />
+      <Text>{title}</Text>
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
--- src/screens/ImageScreen.jsx
+++ src/screens/ImageScreen.jsx
@@ -5,7 +5,6 @@ import ImageDetail from '../components/ImageDetail'
 const ImageScreen = () => {
   return (
     <View>
-      <Text>Image Screen</Text>
       <ImageDetail
         title='Forest'
         imageURL='https://images.unsplash.com/photo-1448375240586-882707db888b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80'
