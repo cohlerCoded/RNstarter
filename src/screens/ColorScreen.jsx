f06de8585b11ba4bc9578c34f029e78e8a3ee886
cohlerCoded <jeffcohler@cohlercoded.com>
2021-07-22 14:55:49 -0700

    generate a random color

--- src/screens/ColorScreen.jsx
+++ src/screens/ColorScreen.jsx
@@ -2,9 +2,16 @@ import React, { useState } from 'react'
 import { View, Text, StyleSheet, Button } from 'react-native'
 
 const ColorScreen = () => {
+  const randomRgb = () => {
+    const r = Math.floor(Math.random() * 256)
+    const g = Math.floor(Math.random() * 256)
+    const b = Math.floor(Math.random() * 256)
+    return `rgb(${r}, ${g}, ${b})`
+  }
   return (
     <View>
-      <Text>ColorScreen</Text>
+      <Button title='Add a Color' />
+      <View style={{ height: 100, width: 100, backgroundColor: randomRgb() }} />
     </View>
   )
 }
