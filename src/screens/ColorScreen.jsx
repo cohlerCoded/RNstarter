59280ffe6adc3b266d8e76ad63c316c2c40468ed
cohlerCoded <jeffcohler@cohlercoded.com>
2021-07-23 15:05:09 -0700

    adding random colors but used map(BAD)

--- src/screens/ColorScreen.jsx
+++ src/screens/ColorScreen.jsx
@@ -2,16 +2,26 @@ import React, { useState } from 'react'
 import { View, Text, StyleSheet, Button } from 'react-native'
 
 const ColorScreen = () => {
+  const [colors, setColors] = useState([])
+
+  console.log(colors)
+
   const randomRgb = () => {
     const r = Math.floor(Math.random() * 256)
     const g = Math.floor(Math.random() * 256)
     const b = Math.floor(Math.random() * 256)
     return `rgb(${r}, ${g}, ${b})`
   }
+
   return (
     <View>
-      <Button title='Add a Color' />
-      <View style={{ height: 100, width: 100, backgroundColor: randomRgb() }} />
+      <Button
+        title='Add a Color'
+        onPress={() => setColors([...colors, randomRgb()])}
+      />
+      {colors.map((color) => (
+        <View style={{ height: 100, width: 100, backgroundColor: color }} />
+      ))}
     </View>
   )
 }
