c5ee6a7c7e00ae5cae9e72b4df713c07ae918372
cohlerCoded <jeffcohler@cohlercoded.com>
2021-07-23 15:54:20 -0700

    passing callbacks to children

--- src/components/ColorCounter.jsx
+++ src/components/ColorCounter.jsx
@@ -1,12 +1,12 @@
 import React, { useState } from 'react'
 import { View, FlatList, StyleSheet, Button, Text } from 'react-native'
 
-const ColorCounter = ({ color }) => {
+const ColorCounter = ({ color, onIncrease, onDecrease }) => {
   return (
     <View>
       <Text>{color}</Text>
-      <Button title={`Increase ${color}`}></Button>
-      <Button title={`Decrease ${color}`}></Button>
+      <Button onPress={onIncrease} title={`Increase ${color}`}></Button>
+      <Button onPress={onDecrease} title={`Decrease ${color}`}></Button>
     </View>
   )
 }
--- src/screens/SquareScreen.jsx
+++ src/screens/SquareScreen.jsx
@@ -1,12 +1,31 @@
 import React, { useState } from 'react'
 import { View, FlatList, StyleSheet, Button, Text } from 'react-native'
 import ColorCounter from '../components/ColorCounter'
+
 const SquareScreen = () => {
+  const [red, setRed] = useState(0)
+  const [blue, setBlue] = useState(0)
+  const [green, setGreen] = useState(0)
+  console.log('Red = ', red)
+  console.log('Blue = ', blue)
+  console.log('Green = ', green)
   return (
     <View>
-      <ColorCounter color='Red' />
-      <ColorCounter color='Blue' />
-      <ColorCounter color='Green' />
+      <ColorCounter
+        color='Red'
+        onIncrease={() => setRed(red + 1)}
+        onDecrease={() => setRed(red - 1)}
+      />
+      <ColorCounter
+        color='Blue'
+        onIncrease={() => setBlue(blue + 1)}
+        onDecrease={() => setBlue(blue - 1)}
+      />
+      <ColorCounter
+        color='Green'
+        onIncrease={() => setGreen(green + 1)}
+        onDecrease={() => setGreen(green - 1)}
+      />
     </View>
   )
 }
