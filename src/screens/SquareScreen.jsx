2ea4138f4556876b7dc141f4ce5f93158ec0b0f2
cohlerCoded <jeffcohler@cohlercoded.com>
2021-07-26 22:38:29 -0700

    creating a reducer

--- src/screens/SquareScreen.jsx
+++ src/screens/SquareScreen.jsx
@@ -2,37 +2,46 @@ import React, { useState } from 'react'
 import { View, FlatList, StyleSheet, Button, Text } from 'react-native'
 import ColorCounter from '../components/ColorCounter'
 
+//using reducer
+
+const reducer = (state, action) => {
+  switch (action.colorToChange) {
+    case 'red':
+      return { ...state, red: state.red + action.amount }
+    case 'blue':
+      return { ...state, blue: state.blue + action.amount }
+    case 'green':
+      return { ...state, green: state.green + action.amount }
+    default:
+      return state
+  }
+}
+const COLOR_ADJUST_NUMBER = 15
+
 const SquareScreen = () => {
-  const [red, setRed] = useState(0)
-  const [blue, setBlue] = useState(0)
-  const [green, setGreen] = useState(0)
-
-  const COLOR_ADJUST_NUMBER = 15
-
+  const [state, dispatch] = useReducer(reducer, { red: 0, green: 0, blue: 0 })
   const setColor = (color, change) => {
     if (color + change > 255) return 255
     if (color + change < 0) return 0
     else return color + change
   }
 
-  console.log('test')
-
   return (
     <View>
       <ColorCounter
         color='Red'
-        onIncrease={() => setRed(setColor(red, COLOR_ADJUST_NUMBER))}
-        onDecrease={() => setRed(setColor(red, -1 * COLOR_ADJUST_NUMBER))}
+        onIncrease={() => }
+        onDecrease={() => }
       />
       <ColorCounter
         color='Blue'
-        onIncrease={() => setBlue(setColor(blue, COLOR_ADJUST_NUMBER))}
-        onDecrease={() => setBlue(setColor(blue, -1 * COLOR_ADJUST_NUMBER))}
+        onIncrease={() => }
+        onDecrease={() => }
       />
       <ColorCounter
         color='Green'
-        onIncrease={() => setGreen(setColor(green, COLOR_ADJUST_NUMBER))}
-        onDecrease={() => setGreen(setColor(green, -1 * COLOR_ADJUST_NUMBER))}
+        onIncrease={() => }
+        onDecrease={() => }
       />
       <View
         style={{
@@ -44,6 +53,49 @@ const SquareScreen = () => {
     </View>
   )
 }
+
+// const SquareScreen = () => {
+//   const [red, setRed] = useState(0)
+//   const [blue, setBlue] = useState(0)
+//   const [green, setGreen] = useState(0)
+
+//   const COLOR_ADJUST_NUMBER = 15
+
+//   const setColor = (color, change) => {
+//     if (color + change > 255) return 255
+//     if (color + change < 0) return 0
+//     else return color + change
+//   }
+
+//   console.log('test')
+
+//   return (
+//     <View>
+//       <ColorCounter
+//         color='Red'
+//         onIncrease={() => setRed(setColor(red, COLOR_ADJUST_NUMBER))}
+//         onDecrease={() => setRed(setColor(red, -1 * COLOR_ADJUST_NUMBER))}
+//       />
+//       <ColorCounter
+//         color='Blue'
+//         onIncrease={() => setBlue(setColor(blue, COLOR_ADJUST_NUMBER))}
+//         onDecrease={() => setBlue(setColor(blue, -1 * COLOR_ADJUST_NUMBER))}
+//       />
+//       <ColorCounter
+//         color='Green'
+//         onIncrease={() => setGreen(setColor(green, COLOR_ADJUST_NUMBER))}
+//         onDecrease={() => setGreen(setColor(green, -1 * COLOR_ADJUST_NUMBER))}
+//       />
+//       <View
+//         style={{
+//           height: 200,
+//           width: 200,
+//           backgroundColor: `rgb(${red}, ${green}, ${blue})`,
+//         }}
+//       />
+//     </View>
+//   )
+// }
 
 const styles = StyleSheet.create({})
 
