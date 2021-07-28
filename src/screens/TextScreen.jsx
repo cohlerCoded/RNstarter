6a474dbe92a1e42bb5d4de32be3c3dbe23b0ec49
cohlerCoded <jeffcohler@cohlercoded.com>
2021-07-28 13:14:36 -0700

    password exercise

--- src/screens/TextScreen.jsx
+++ src/screens/TextScreen.jsx
@@ -3,6 +3,7 @@ import { View, StyleSheet, Text, TextInput } from 'react-native'
 
 const TextScreen = () => {
   const [name, setName] = useState('')
+  const [password, setPassword] = useState('')
   return (
     <View>
       <Text>Enter Name:</Text>
@@ -13,7 +14,18 @@ const TextScreen = () => {
         value={name}
         onChangeText={(newName) => setName(newName)}
       />
-      <Text>Hi, {name}, nice to meet you!</Text>
+      <Text>My name is {name}</Text>
+      <Text>Enter Password:</Text>
+      <TextInput
+        style={styles.input}
+        autoCapitalize='none'
+        autoCorrect={false}
+        value={password}
+        onChangeText={(newPassword) => setPassword(newPassword)}
+      />
+      {password.length > 0 && password.length < 6 && (
+        <Text>Password must be longer than 5 characters</Text>
+      )}
     </View>
   )
 }
