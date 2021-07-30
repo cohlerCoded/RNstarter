1dcbe26b720cdcec59d1c5506acf14bd5137860e
cohlerCoded <jeffcohler@cohlercoded.com>
2021-07-30 08:42:42 -0700

    absoluteFillObject

--- src/screens/BoxScreen.jsx
+++ src/screens/BoxScreen.jsx
@@ -24,7 +24,7 @@ const styles = StyleSheet.create({
     borderWidth: 3,
     borderColor: 'red',
     fontSize: 18,
-    right: 10,
+    ...StyleSheet.absoluteFillObject,
   },
   textThreeStyle: {
     borderWidth: 3,
