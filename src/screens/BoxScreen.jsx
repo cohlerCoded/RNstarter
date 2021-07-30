575c5d1f50e3c3b413e1ceea92a9c4d77afdc783
cohlerCoded <jeffcohler@cohlercoded.com>
2021-07-29 20:22:22 -0700

    flexDirection

--- src/screens/BoxScreen.jsx
+++ src/screens/BoxScreen.jsx
@@ -14,7 +14,9 @@ const styles = StyleSheet.create({
   viewStyle: {
     borderWidth: 3,
     borderColor: 'black',
-    alignItems: 'center',
+    flexDirection: 'row',
+    height: 200,
+    alignItems: 'flex-end',
   },
   textStyle: {
     borderWidth: 3,
